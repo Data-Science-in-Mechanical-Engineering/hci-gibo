12ed05c51b54063255e13a3cefa4da2438a6a7c6
u-mask <107872243+u-mask@users.noreply.github.com>
2023-10-27 13:56:31 +0800

    fix typo

--- gym-franka-pole/gym_franka_pole/__init__.py
+++ gym-franka-pole/gym_franka_pole/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='franka_pole-v0',
-    entry_point='gym_franka_pole.envs:franka_poleex_Env',
+    entry_point='gym_franka_pole.envs:franka_pole_Env',
 )
